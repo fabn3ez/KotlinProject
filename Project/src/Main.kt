9973296540db36dd3f3deabe61c259c22452ba44
Default404 <baraka.onserio@strathmore.edu>
2026-09-03 15:08:17 +0300

    Variables & Constants

--- Project/src/Main.kt
+++ Project/src/Main.kt
@@ -1,14 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+// Concept Note: https://docs.google.com/document/d/1ZvSlpqKyDVVux8d41GMRsrZhY8nXpwsVDHzVy_g51DQ/edit?usp=sharing
+// GROUP 5:
+// 189722 - Brian Koech
+// 179486 - Kamure Beyonce Kabura
+// 191627 - Onserio Baraka Osumo
+// 191737 - Bethuel Kilonzo
+// 176676 - Daniella Wanjohi
+// 189808 - Ian Simiyu
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+// Compile-time constant
+const val MAX_USERS = 100
+fun main() {
+    println("========================================")
+    println(" REAL ESTATE PROPERTY LISTING SYSTEM")
+    println("========================================")
+    println("Welcome! This system helps agents manage property listings.")
+
+    // val: cannot be changed
+    val appName = "My Listing App"
+    // appName = "New Name" // Error: Val cannot be reassigned
+
+    // var: can be changed
+    var activeUsers = 10
+    activeUsers = 15 // Reassignment works perfectly
+
+    val releaseYear: Int = 2026
+    var isPublished: Boolean = false
+
+    println("App: $appName ($releaseYear)")
+    println("Users: $activeUsers / $MAX_USERS")
 }
